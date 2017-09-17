b9037b87676977775abd7c5be8ec32869668ebbd
Bing <qiaoyanbing1@163.com>
2017-09-17 21:00:37 +0800

    最长的回文
    算法没怎么看懂，下次要认真看

---
+++ 5.Longest Palindromic Substring/Longest Palindromic Substring.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        self.maxlen = 0
+        self.retstr = ''
+        if len(s) < 2:
+            return s
+        for i in range(len(s)):
+            self.__find_palindrome(s, i, i) #奇回文
+            self.__find_palindrome(s, i, i+1) #偶回文
+        return self.retstr
+
+
+    def __find_palindrome(self, s, j, k):
+        while j >= 0 and k < len(s) and s[j] == s[k]:
+            j -= 1
+            k += 1
+        if self.maxlen < k - j + 1:
+            self.maxlen = k - j + 1
+            self.retstr = s[j+1:k]
