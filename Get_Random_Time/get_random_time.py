024edf17b5bffb8de6ea5d8c42baed6ad876e91b
Bing <qiaoyanbing1@163.com>
2017-12-07 00:00:00 +0800

    添加的提醒

--- Get_Random_Time/get_random_time.py
+++ Get_Random_Time/get_random_time.py
@@ -4,17 +4,21 @@ import random
 import pymysql
 import time
 
-db = pymysql.connect(host='xxxx', user='xxxx', passwd='xxxx', db='xxxxx',
+db = pymysql.connect(host='xxxxx', user='xxxx', passwd='xxxxx', db='xxxxx',
                                   charset='utf8')
 cursor = db.cursor()
 def change_time(result):
     t = int(time.time())
     num = math.ceil(len(result) * 0.2)
     rs_nums = random.sample(result, num)
+    print('随机的获取列表：{}'.format(rs_nums))
     for rs_num in rs_nums:
         if abs(t - rs_num[1]) > 1800:
             rs_time = t - random.randint(500, 1800)
+            print('修改的id:{},时间戳修改为：{}'.format(rs_num[0], rs_time))
             save_to_mysql(rs_num[0], rs_time)
+        else:
+            print('id为{}满足条件不需要修改'.format(rs_num[0]))
 
 def save_to_mysql(id,activity):
     sql = "update location set activity={} where id ={}".format(activity,id)
@@ -22,6 +26,7 @@ def save_to_mysql(id,activity):
         cursor.execute(sql)
         db.commit()
         print('id为{}修改成功'.format(id))
+        print('------------------------------')
     except Exception as e:
         print(e)
         print('修改失败')
