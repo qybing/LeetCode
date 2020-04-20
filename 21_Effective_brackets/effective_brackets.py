dc41cd8d8a2ffce94a1f090a0b9d5872e9dbf8f5
qybing <qiaoyanbing1@163.com>
2020-04-20 14:29:03 +0800

    Revert "Revert "括号有效性""
    This reverts commit 1a7780ab

---
+++ 21_Effective_brackets/effective_brackets.py
@@ -0,0 +1,16 @@
+#! python3
+# _*_ coding: utf-8 _*_
+# @Time : 2020/4/20 14:03 
+# @Author : Jovan
+# @File : effective_brackets.py
+# @desc : 括号的有效性， 采用栈的方式来
+class Solution:
+    def isVaild(self, s: str) -> bool:
+        d = {'{': '}', '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for i in s:
+            if i in d:
+                stack.append(i)
+            elif d[stack.pop()] != i:
+                return False
+        return len(stack) == 1
