d420600e060695484964b9b81e7b8f657613a0be
qybing <qiaoyanbing1@163.com>
2020-05-15 18:15:42 +0800

    更简单的方法

--- 10. Regular Expression Matching/10. Regular Expression Matching.py
+++ 10. Regular Expression Matching/10. Regular Expression Matching.py
@@ -1,22 +1,34 @@
+'''
+
+给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+
+说明:
+
+s 可能为空，且只包含从 a-z 的小写字母。
+p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+示例 1:
+
+输入:
+s = "aa"
+p = "a"
+输出: false
+解释: "a" 无法匹配 "aa" 整个字符串。
+'''
 import re
 
 
-def isMatch(s,p):
-    # if s == "":
-    #     return True
-    pattern = re.compile(r'{}'.format(p))
-    result = pattern.match(s)
-    # print(result.group()==s)
-    # print(s)
-    if result:
-        if result.group()==s :
-            return True
-        else:
-            return False
+def isMatch(s, p):
+    res = re.match(p, s)
+    if res:
+        return res.end() == len(s)
     else:
         return False
-        # print(result.group())
-if __name__ == '__main__':
-    s = "aa"
-    p = "a"
-    print(isMatch(s,p))
+
+
+s = "aa"
+p = "a*"
+print(isMatch(s, p))
